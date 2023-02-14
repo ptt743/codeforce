e5007a1201c7c27011c5b5b4a783a220a14e89d1
ptt743 <55840736+ptt743@users.noreply.github.com>
2023-02-15 00:38:18 +0700

    update

--- problemsets/202A.cpp
+++ problemsets/202A.cpp
@@ -11,15 +11,32 @@ using namespace std;
 /*
 */
 void solve(){
-	
+  string a;
+  cin>>a;
+  int len,i,j,al[27];
+  len =  strlen(a.c_str());
+  for(i=1;i<=26;i++){
+    al[i] = 0;
+  }
+  for(i=0;i<len;i++){
+    al[int(a[i])-96]++;
+  }
+  for(i=26;i>=1;i--){
+    if(al[i]>0){
+        for(j=0;j<al[i];j++){
+            cout<<char(96+i);
+        }
+      exit(0);
+    }
+  }	
 }
  
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
     int t;
-    cin >> t;
-    while(t--){
+  //  cin >> t;
+    //while(t--){
         solve();
-    }
+    //}
     return 0;
 }
